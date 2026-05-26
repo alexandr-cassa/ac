ffcca47c07b10c44633c1c6856a29efe53e18a60
Alexandr Cassa <alexandr.cassa@gmail.com>
2026-05-26 19:15:22 +0300

    Test jenkins job

--- jenkinsfiles/TestJob.groovy
+++ jenkinsfiles/TestJob.groovy
@@ -1,18 +1,58 @@
 pipeline {
     agent any
 
+    environment {
+        APP_NAME = 'g2048'
+        MANIFEST_FILE = 'g2048/deployment.yaml'
+        GIT_BRANCH = 'master'
+    }
+
     stages {
-        stage('Build image') {
+        stage('Get commit hash') {
             steps {
                 script {
-                    def commitHash = sh(
+                    env.COMMIT_HASH = sh(
                             script: 'git rev-parse --short HEAD',
                             returnStdout: true
                     ).trim()
 
-                    sh "docker build -t g2048:${commitHash} ."
+                    env.IMAGE_TAG = "${env.APP_NAME}:${env.COMMIT_HASH}"
                 }
             }
+        }
+
+        stage('Build image') {
+            steps {
+                sh 'docker build -f Dockerfile -t ${IMAGE_TAG} . '
+            }
+        }
+
+        stage('Clone repo with manifests') {
+            steps {
+                sh 'git clone https://github.com/alexandr-cassa/ac-k8s.git'
+            }
+        }
+
+        stage('Update Argo manifest') {
+            steps {
+                sh '''
+                  cd ac-k8s
+                  yq -i '(.spec.template.spec.containers[] | select(.image | test("^g2048:")) | .image) = strenv(IMAGE_TAG)' "$MANIFEST_FILE"
+
+                  git config user.name "jenkins"
+                  git config user.email "jenkins@example.com"
+
+                  git add ${MANIFEST_FILE}
+                  git commit -m "Update ${APP_NAME} image to ${COMMIT_HASH}" || echo "No changes to commit"
+                  git push origin ${GIT_BRANCH}
+                '''
+            }
+        }
+    }
+
+    post {
+        always {
+            sh 'rm -rf ac-k8s'
         }
     }
 }
